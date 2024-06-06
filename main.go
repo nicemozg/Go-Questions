4bd42756bba97d2729a1a4f0ad1ebd6a1ebac5d8
nicemozg <nice.mozg@gmail.com>
2024-06-07 00:39:14 +0500

    Update README.md

--- main.go
+++ main.go
@@ -1,34 +1,37 @@
 package main
 
 import (
-	"fmt"
-	"time"
+	"errors"
+	"io"
+	"log"
+	"os"
 )
 
 func main() {
-	// Создаем два канала
-	ch1 := make(chan string)
-	ch2 := make(chan string)
-
-	// Горутина отправляет "Hello" в канал ch1 через 2 секунды
-	go func() {
-		time.Sleep(2 * time.Second)
-		ch1 <- "Hello"
-	}()
-
-	// Горутина отправляет "World" в канал ch2 через 1 секунду
-	go func() {
-		time.Sleep(1 * time.Second)
-		ch2 <- "World"
-	}()
-
-	// Используем оператор select для ожидания и получения значений из каналов
-	for i := 0; i < 2; i++ {
-		select {
-		case msg1 := <-ch1:
-			fmt.Println("Received from ch1:", msg1)
-		case msg2 := <-ch2:
-			fmt.Println("Received from ch2:", msg2)
-		}
+	// Открытие файла в режиме добавления или создание нового файла, если он не существует
+	file, err := os.OpenFile("app.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
+	if err != nil {
+		log.Fatal("Cannot open log file:", err)
 	}
+	defer file.Close()
+
+	// Установка многопоточного вывода в файл и в консоль
+	multiWriter := io.MultiWriter(file, os.Stdout)
+	log.SetOutput(multiWriter)
+
+	// Логирование сообщений
+	log.Println("Программа запущена")
+	log.Printf("Значение переменной: %d", 42)
+
+	// Логирование ошибки
+	err = someFunction()
+	if err != nil {
+		log.Printf("Ошибка в someFunction: %v", err)
+	}
+}
+
+// Пример функции, которая может возвращать ошибку
+func someFunction() error {
+	// Имитация ошибки
+	return errors.New("произошла ошибка в someFunction")
 }
